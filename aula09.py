6ee33078a5c602bd433f2fc0a244b2b4ae3100f8
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-15 17:54:40 -0300

    Create aula09.py

---
+++ aula09.py
@@ -0,0 +1,16 @@
+frase = "Curso em Vídeo Python"
+print(frase)
+print(frase[3:14])
+print(frase[:12])
+print(frase[:14])
+print(frase[9::3])
+print(frase[1:15:2])
+print(frase[::2])
+print(frase.count("o"))
+print(len(frase))
+print(frase.replace("Python" , "Android"))
+print(frase.split())
+dividido = frase.split()
+print(dividido[2])
+print(dividido[2][3])
+
