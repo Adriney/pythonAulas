8756237216e29447cdd13486d99746578530db81
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-17 21:11:02 -0300

    Create aula010d.py

---
+++ aula010d.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("digite a segunda nota: "))
+m = (n1+n2)/2
+print("A sua média foi {:.1f}".format(m))
+if m >=6.0: #não esquecer os dois pontos
+    print("Sua média foi boa! PARABÉNS!")
+else:
+    print("Sua média foi ruim! ESTUDE MAIS!")
