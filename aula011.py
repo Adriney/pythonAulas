2b5cd786efdd423fdc420875cc6ced0940d97aaf
Adriney <142994789+Adriney@users.noreply.github.com>
2024-04-17 21:11:11 -0300

    Create aula011.py

---
+++ aula011.py
@@ -0,0 +1,5 @@
+print("\033[7;37;40mOlá mundo!\033[m ")
+print(" \033[32mTeste\033[m \033[31mde\033[m \033[36mcores\033[m")
+a = 3
+b = 5
+print("Os valores são  \033[32m{}\033[m e \033[31m{}\033[m !!!".format(a,b))
